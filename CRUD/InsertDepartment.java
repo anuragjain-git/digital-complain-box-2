9ea876234e4a5af119fe7ebe288e5716d1f33659
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2025-06-16 19:24:43 +0530

    Update InsertDepartment.java

--- CRUD/InsertDepartment.java
+++ CRUD/InsertDepartment.java
@@ -4,21 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import model.Department;
-import util.DatabaseUtil;
+import digital_complain_box.Department;
 
 public class InsertDepartment {
 	public void insertDepartment(Department department) {
         String sql = "INSERT INTO departments (dept_name, description) VALUES (?, ?)";
 
-        try (Connection conn = DatabaseUtil.getConnection();
+        try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, department.getDeptName());
             stmt.setString(2, department.getDescription());
 
             stmt.executeUpdate();
-            System.out.println("✅ Department inserted.");
+            System.out.println("Department inserted.");
 
         } catch (SQLException e) {
             e.printStackTrace();
