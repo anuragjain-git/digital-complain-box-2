d6cf251e78f3fe177fb0802e08bb3a309d96fa57
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2025-06-18 15:29:05 +0530

    Create CollectionsStore.java

---
+++ Main/digital_complain_box/CollectionsStore.java
@@ -0,0 +1,10 @@
+package digital_complain_box;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsStore {
+    public static final List<User> userList = new ArrayList<>();
+    public static final List<Complaint> complaintList = new ArrayList<>();
+    public static final List<Response> responseList = new ArrayList<>();
+}
