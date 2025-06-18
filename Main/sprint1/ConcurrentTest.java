c2acedbfab76aec254e9f5444d0ade1ba65069d1
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2025-06-18 22:14:16 +0530

    Create ConcurrentTest.java

---
+++ Main/sprint1/ConcurrentTest.java
@@ -0,0 +1,30 @@
+package sprint1;
+
+import java.sql.*;
+
+public class ConcurrentTest {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new ComplaintSubmitter("anuragjain", "IT", "Software", "Website broken", "Need Help with software"));
+        Thread t2 = new Thread(new ComplaintSubmitter("anushka", "Finance", "Payroll", "Salary issue", "Salary not credited"));
+        Thread t3 = new Thread(new ComplaintSubmitter("aviral", "Finance", "Software", "Software not working", "Screen flickering"));
+        Thread t4 = new Thread(new ComplaintSubmitter("anuragjain", "IT", "Internet", "Wi-Fi not working", "Should be blocked"));
+
+        // Start all threads
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        // Wait for all to finish
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads completed.");
+    }
+}
